d9797fb2d79d78cd51e1537a013179f2193c9e63
jatin anand <jtanand1407@gmail.com>
2024-09-01 03:00:09 +0530

    Added login apis

--- index.js
+++ index.js
@@ -2,14 +2,61 @@ const { dbConnection } = require("./db");
 const { ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const jwt_key =
+  "3a1f9a3c7c4d8d91a1b9e817c3f29a65bf4821cba8ea6723d58e71d26e0135d4a5fd6e7c68e78e45f2bb9f3f0e6a6d12";
+
 const PORT = process.env.PORT || 3030;
 
-app.get("/", async (req, res) => {
+// Login Apis
+
+app.post("/register", async (req, res) => {
+  const db = await dbConnection();
+  const collection = db.collection("qaUsers");
+  const result = await collection.findOne(req.body);
+  if (result) {
+    return res.send("User Already Exists");
+  } else {
+    const addUser = await collection.insertOne(req.body);
+    return res.status(201).send("User Created");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const db = await dbConnection();
+  const collection = db.collection("qaUsers");
+  const { username, password } = req.body;
+  const passCheck = await collection.findOne(req.body);
+  if (!passCheck) {
+    return res.send("Invalid User or Password");
+  }
+  var token = jwt.sign(req.body, jwt_key, { expiresIn: "1m" });
+  return res.json({ token: token });
+});
+
+app.post("/verify", (req, res) => {
+  const token = req.body.token;
+  if(!token)
+  {
+    return res.status(400).send("Token is required");
+  }
+  jwt.verify(token, jwt_key, function (err, decoded) {
+    if (err) {
+      return res.status(401).send("Token Expired");
+    } else {
+      return res.status(200).send("Token Validated");
+    }
+  });
+});
+
+// Home Apis
+
+app.get("/home", async (req, res) => {
   try {
     const db = await dbConnection();
     const collection = db.collection("qaCollection");
@@ -45,7 +92,7 @@ app.post("/cart", async (req, res) => {
   }
 });
 
-app.put("/", async (req, res) => {
+app.put("/home", async (req, res) => {
   try {
     const db = await dbConnection();
     const collection = db.collection("qaCollection");
@@ -60,7 +107,7 @@ app.put("/", async (req, res) => {
   }
 });
 
-app.delete("/", async (req, res) => {
+app.delete("/home", async (req, res) => {
   try {
     const db = await dbConnection();
     const collection = db.collection("qaCollection");
@@ -77,7 +124,7 @@ app.delete("/cart/:id", async (req, res) => {
     const db = await dbConnection();
     const collection = db.collection("qaCart");
     const deleteId = req.params.id;
-    const item = await collection.findOne({'_id': new ObjectId(deleteId)});
+    const item = await collection.findOne({ _id: new ObjectId(deleteId) });
     const data = await collection.deleteOne(item);
     res.send("Deleted Successfully!!!");
   } catch (error) {
